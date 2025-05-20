683e9cf5e9a26d257baf94f3bdaaf0ca64f22974
Ngoding Bang <ngodingbang@gmail.com>
2025-05-20 14:13:55 +0700

    chore: add optional() attribute on app.config

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,7 +5,7 @@ export const appConfigSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'staging', 'production'])
     .default('development'),
-  PORT: z.number().step(1).min(1).max(9999).default(3000),
+  PORT: z.number().step(1).min(1).max(9999).optional().default(3000),
 });
 
 export class AppConfig extends Config(appConfigSchema) {}
